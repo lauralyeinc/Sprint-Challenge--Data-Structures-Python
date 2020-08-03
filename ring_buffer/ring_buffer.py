0ce7848553aedee22beb560b63220f81c1e5403c
Laura <theimer.laura@gmail.com>
2020-08-02 21:28:52 -0400

    ring_buffer 6 tests pass OK

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
+
+#https://towardsdatascience.com/circular-queue-or-ring-buffer-92c7b0193326
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
+        self.index = 0 
 
     def append(self, item):
-        pass
+        if len(self.data) == self.capacity:
+            self.data[self.index] = item
+        else:
+            self.data.append(item)
+
+        self.index = (self.index + 1) % self.capacity
+        #pass
 
     def get(self):
-        pass
+        return self.data
+        #pass
